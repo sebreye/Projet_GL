e50cb835d11434d2f72557f0ca3c382c0d6c7b6e
sebastian Reyes <sebreyes222gmail.com>
2023-01-09 00:07:19 +0100

    ajout Qenigma

--- public/js/modules/boss.js
+++ public/js/modules/boss.js
@@ -1,9 +1,22 @@
 import enigma from "./enigma";
 
 class Boss {
-  constructor(name, health, attack) {
+    constructor(name, health, attack) {
     this.name = name;
     this.health = health;
     this.maxHealth = health;
     this.attack = attack;
-  }
+}
+    Qenigma() {
+    const enigme = enigma[Math.floor(Math.random() * enigma.length)];
+    console.log(`${this.name} vous pose une énigme : ${enigme.question}`);
+    for (let i = 0; i < 3; i++) {
+        const answer = prompt("Quelle est la réponse ?");
+        if (answer.toLowerCase() === enigme.answer.toLowerCase()) {
+        console.log("Bonne réponse ! Vous avez vaincu le boss.");
+        return;
+        }
+    }
+    console.log("Vous avez échoué à l'énigme. Vous avez perdu.");
+    }
+}
