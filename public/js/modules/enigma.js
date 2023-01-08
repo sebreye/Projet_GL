89ff709c1d22295391f4ed4b1782a6ae92867291
sebastian Reyes <sebreyes222gmail.com>
2023-01-09 00:02:23 +0100

    ajout enigma

---
+++ public/js/modules/enigma.js
@@ -0,0 +1,9 @@
+let enigma = [
+    { question: "Une fois que l'on me prononce, je n'existe plus. Qui suis-je ?", reponse: "Le silence" },
+    { question: "Je suis d'eau, je suis d'air, et je suis d'électricité. Qui suis-je ?", reponse: "L'éclair" },
+    { question: "Quel heure est-il ?", answer: "La fonction Date.now() permet de récupérer l'heure actuelle en timestamp, il faut donc l'utiliser pour afficher l'heure actuelle de manière lisible." },
+    { question: "Quel est l'indice du premier 'i' de cette question ?", reponse: "8" },
+    { question: "Que fait retourne Math.floor(1.3 * 10) ?", reponse: "13" },
+];
+
+export default enigma;
