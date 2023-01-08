cef17beef7d9b895e6b5cffbc37c62d9a97fa45a
sebastian Reyes <sebreyes222gmail.com>
2023-01-09 00:10:59 +0100

    ajout defense et attaque

--- public/js/modules/hero.js
+++ public/js/modules/hero.js
@@ -5,3 +5,13 @@ class Hero {
         this.maxHealth = health;
         this.attack = attack;
     }
+    defense() {
+        this.health += this.maxHealth * 0.5;
+        this.attaque -= this.attaque * 0.5;
+        }
+    
+    attaque() {
+        this.health -= this.maxHealth * 0.25;
+        this.attaque += this.attaque * 0.4;
+        }
+    }
