8b9e54fcc9dafab6287e833797adf4c1f919b767
sebastian Reyes <sebreyes222gmail.com>
2023-01-09 00:15:01 +0100

    ajout fin mage

--- public/js/modules/mage.js
+++ public/js/modules/mage.js
@@ -6,4 +6,13 @@ class Mage extends Hero {
         this.mana = Math.floor(Math.random() * 3) + 7;
     }
 
-    
+    attack() {
+        if (this.mana < 2) {
+            console.log(`${this.name} n'a plus assez de mana pour attaquer. Il passe son tour.`);
+                this.mana += 7;
+            return;
+        }
+        super.attack();
+        this.mana -= 2;
+        }
+    }
