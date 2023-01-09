114dfe872a61604e51d1887a67a316f1cc64a834
sebastian Reyes <sebreyes222gmail.com>
2023-01-09 09:58:29 +0100

    ajout var fct

--- public/js/modules/fonction.js
+++ public/js/modules/fonction.js
@@ -1,3 +1,8 @@
+import Guerrier from "./guerrier.js";
+import Mage from "./mage.js";
+import Archer from "./archer.js";
+import Boss from"./boss.js";
+
 function askstart(reponse) {
     let reponse = prompt("Voulez-vous démarrer le jeu ? (oui / non)")
     if(reponse.toLowerCase() === 'oui'){
@@ -10,3 +15,14 @@ function askstart(reponse) {
 function name(params) {
     
 }
+
+
+
+
+
+let guerrier = new Guerrier('Guerrier', 100, 25)
+let mage = new Mage('Mage', 100, 15)
+let archer = new Archer('Archer', 100, 10)
+let Sauron = new Boss('Sauron', 250, 30)
+let Chronos = new Boss('Chronos', 300, 35)
+let Lilith = new Boss('Lilith', 350, 40)
